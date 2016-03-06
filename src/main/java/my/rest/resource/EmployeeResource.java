61ca5dd4e0b0520020e4d7fc352fe22584d7aa93
Smruti Ranjan Sahoo <smrutis@yahoo-inc.com>
2016-03-06 01:33:30 -0800

    added api description

--- src/main/java/my/rest/resource/EmployeeResource.java
+++ src/main/java/my/rest/resource/EmployeeResource.java
@@ -2,17 +2,19 @@ package my.rest.resource;
 
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 import my.rest.model.Employee;
 
-@Path("/v1/employees")
-@Api
+@Path("/v1/employee")
+@Api("employees")
 public class EmployeeResource {
 
     @POST
+    @ApiOperation("Add a new employee")
     public Employee create(Employee employee){
         return employee;
     }
--- src/main/java/my/rest/resource/GreetingsResource.java
+++ src/main/java/my/rest/resource/GreetingsResource.java
@@ -1,6 +1,7 @@
 package my.rest.resource;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,13 +9,14 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.SecurityContext;
 
 @Path("/hi")
-@Api
+@Api("hi")
 public class GreetingsResource {
 
     @Context
     private SecurityContext securityContext;
 
     @GET
+    @ApiOperation("Welcome to REST")
     public String hello() {
         System.out.println(securityContext);
         System.out.println(securityContext.getUserPrincipal());
