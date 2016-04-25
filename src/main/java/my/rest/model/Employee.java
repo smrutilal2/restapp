594a09401dd45087f80a93752c8e3b3939110026
Smruti Ranjan Sahoo <smrutis@yahoo-inc.com>
2016-04-24 21:27:11 -0700

    jackson integrated

--- src/main/java/my/rest/model/Employee.java
+++ src/main/java/my/rest/model/Employee.java
@@ -1,4 +1,15 @@
 package my.rest.model;
 
+
 public class Employee {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/my/rest/resource/EmployeeResource.java
+++ src/main/java/my/rest/resource/EmployeeResource.java
@@ -4,8 +4,12 @@ package my.rest.resource;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import my.rest.model.Employee;
 
@@ -15,8 +19,11 @@ public class EmployeeResource {
 
     @POST
     @ApiOperation("Add a new employee")
-    public Employee create(Employee employee){
-        return employee;
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Employee employee) {
+        System.out.println(String.format("%s created", employee.getName()));
+        return Response.status(Response.Status.CREATED).entity(employee).build();
     }
 
 }
